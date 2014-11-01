d7a063fe935d2b0aed7b5d6ab85d26a910cdbbce
l0ner <pwslts@gmail.com>
2014-11-01 13:26:29 +0100

    setup.py changes?

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from distutils.core import setup
 
-    config = {
-            'description': 'bcut - Print selected parts of lines',
-            'author': 'Pawel <l0ner> Soltys',
-            'url': 'http://github.com/l0ner/bcut',
-            'author_email': 'pwslts@gmail.com',
-            'version': '0.1',
-            'install_requires': [],
-            'packages': ['bcut'],
-            'scripts': [],
-            'name': 'bcut'
-            }
+config = {
+    'name': 'bcut',
+    'packages': ['bcut'],
+    'version': '0.1',
+    'description': 'bcut - Print selected parts of lines',
+    'author': 'Pawel <l0ner> Soltys',
+    'author_email': 'pwslts@gmail.com',
+    'url': 'http://github.com/l0ner/bcut',
+    'keywords': [],
+    'classifiers': [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing",
+        "Topic :: Utilities"
+        ],
+    'long_description': """\""",
+    'install_requires': [],
+    'scripts': [],
+    }
 
-    setup(**config)
+setup(**config)
