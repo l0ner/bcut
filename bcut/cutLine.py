630e9d0d8b0fad8aa4e301da3f671a37ba013a96
l0ner <pwslts@gmail.com>
2014-11-03 16:11:54 +0100

    byte cutting init

--- bcut/cutLine.py
+++ bcut/cutLine.py
@@ -1,4 +1,10 @@
-
+def cutBytes(data, ranges, complement=False, invert=False):
+    out = bytes()
+    for rng in ranges:
+        if rng['end'] == 0:
+            rng['end'] = len(data) - 2
+        out += data[rng['start']-1:rng['end']]
+    return out
 
 def cutLine(data, mode, ranges, complement, invert, sep='\t'):
     """Return selected bytes/characters/fields from data
@@ -11,3 +17,4 @@ def cutLine(data, mode, ranges, complement, invert, sep='\t'):
     invert - count data from the end
     sep - separator to use (default <TAB>) <string>
     """
+    pass
