dbd77bd33ee2fbb901c9a384e527de1e9c13f4ec
l0ner <pwslts@gmail.com>
2014-11-10 13:49:31 +0100

    added license header

--- bcut/cutLine.py
+++ bcut/cutLine.py
@@ -1,3 +1,26 @@
+####################################################################
+#
+# bcut - a better cut utility
+#
+# Copyright (C) 2014 Pawel 'l0ner' Soltys <pwslts@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##########################################################################
+
+''' Cutting functions '''
+
 def cutBytes(data, ranges, invert=False):
     out = bytes()
     if invert:
@@ -46,3 +69,4 @@ def cutFields(data, ranges, invert=False):
 
 def reverseData(data):
     return data[::-1]
+
